8f3047721c3213ffd19c4a9a719b52bd09709382
Damian Schmidt <damian.schmidt97@gmail.com>
2018-12-23 17:55:45 +0100

    Add new functions in Word class and possibility of one game

---
+++ scrapewords.py
@@ -0,0 +1,74 @@
+import sys
+import requests
+import pymongo
+from bs4 import BeautifulSoup
+
+
+def scrape_words():
+    """ Function scrapping English words and descriptions from website and translate them to Polish """
+    list_of_words = []
+    number = 1
+
+    print('Scrapping words is in progress... Please wait a minute.')
+    try:
+        source = requests.get('https://www.vocabulary.com/lists/351211').text
+        soup = BeautifulSoup(source, 'lxml')
+
+    except requests.exceptions.RequestException:
+        print('There is problem with connection to base website. Program will be closed.')
+        sys.exit()
+
+    for table_row in soup.find_all('li', class_=['entry learnable', 'entry']):
+        english_word = table_row.find('a', class_='word dynamictext').text
+        description = table_row.find('div', class_='definition').text
+
+        try:
+            translator_source = requests.get(f'https://www.diki.pl/slownik-angielskiego?q={english_word}').text
+            translator_soap = BeautifulSoup(translator_source, 'lxml')
+
+            polish_word = translator_soap.find('div', class_='dictionaryEntity').ol.li.span.text.strip()
+
+        except requests.exceptions.RequestException:
+            print('There is problem with connection to website. Unable to translate word.')
+            polish_word = 'Translation error!'
+
+        except AttributeError:
+            polish_word = 'Translation error!'
+
+        list_of_words.append({'number': number,
+                              'english_word': english_word,
+                              'description': description,
+                              'polish_word': polish_word})
+
+        number += 1
+    print('Scrapped all words.')
+    return list_of_words
+
+
+def add_to_database(list_of_words):
+    """ Adding list of scraped words to database """
+    my_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    my_db = my_client['python-words']
+    if 'words' in my_db.list_collection_names():
+        my_db['words'].drop()
+
+    my_col = my_db['words']
+    my_col.insert_many(list_of_words)
+
+    print('Adding to database is completed.')
+
+
+def length_of_collection():
+    """ Return length of words collection """
+    my_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    my_db = my_client['python-words']
+    my_col = my_db['words']
+    return my_col.count()
+
+
+def find_word_by_number(number):
+    """ Return record find in collection which has got number equal to argument """
+    my_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    my_db = my_client['python-words']
+    my_col = my_db['words']
+    return my_col.find_one({'number': number})
--- word.py
+++ word.py
@@ -1,20 +1,60 @@
+import random
+import sys
+
 
 class Word:
-    id = 0
-
-    def __init__(self, english_word: chr, description: chr, polish_word: chr):
+    def __init__(self, number: int, english_word: chr, description: chr, polish_word: chr):
+        self.number = number
         self.english_word = english_word
         self.description = description
         self.polish_word = polish_word
 
-        Word.id += 1
-        self.id = Word.id
-
-    def add_to_database(self):
-        pass
-
     def print_info(self):
-        print('English Word: ' + self.english_word + '\n' +
+        """ Method print all fields of object """
+        print('Number: ' + str(self.number) + '\n' +
+              'English Word: ' + self.english_word + '\n' +
               'Description: ' + self.description + '\n' +
-              'Polish Word: ' + self.polish_word + '\n' +
-              'id: ' + str(self.id) + '\n')
+              'Polish Word: ' + self.polish_word + '\n')
+
+    def hide_letters(self):
+        """ Method hide few random letters of word depends on word's length """
+        length = len(self.english_word)
+        num_to_stay = length // 3
+        stay = []
+
+        # Make list of position of characters that you want to hide
+        while num_to_stay != len(stay):
+            letter_ptr = random.randrange(0, length)
+            if letter_ptr not in stay:
+                stay.append(letter_ptr)
+
+        # Hash letters which aren't in stay list
+        new_word = []
+        for i in range(0, length):
+            if i in stay:
+                new_word.append(self.english_word[i])
+            else:
+                new_word.append('-')
+
+        return ''.join(new_word)
+
+    def is_letter_in_word(self, letter, word):
+        """ Return True if letter is in word or False if isn't """
+        for i in range(0, len(self.english_word)):
+            if word[i] != self.english_word[i] and letter == self.english_word[i]:
+                return True
+
+        return False
+
+    def show_letters(self, letter, word):
+        for i in range(0, len(self.english_word)):
+            if word[i] != self.english_word[i] and letter == self.english_word[i]:
+                list_word = list(word)
+                list_word[i] = letter
+                word = ''.join(list_word)
+        if word == self.english_word:
+            print(word)
+            print('WIN!')
+            sys.exit()
+        else:
+            return word
